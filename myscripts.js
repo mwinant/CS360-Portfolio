e394ca2cf570a39ec918eab3e10f8520019dac5c
mwinant <mwinant@nic.edu>
2025-09-25 11:50:42 -0700

    added signup and signin page

--- myscripts.js
+++ myscripts.js
@@ -13,3 +13,16 @@ function loadNavbar() {
             });
         });
 }
+
+function loadFooter() {
+    fetch("footer.html")
+        .then(response => {
+            if (!response.ok) throw new Error("Footer file not found");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        })
+        .catch(err => console.error("Failed to load footer:", err));
+}
+
